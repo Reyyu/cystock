09f26e07951084552c438e1328c43d65dff15501
Reyyu <403325714@qq.com>
2016-04-13 23:51:01 +0800

    Create common.h

---
+++ common.h
@@ -0,0 +1,22 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include<vector>
+#include<string>
+#include<sstream>
+#include<map>
+#include<iostream>
+#include <math.h>
+#include <Windows.h>  
+#include <wininet.h> 
+#include <fstream>
+#include <sstream>
+#include <regex>
+#include <iostream>
+using namespace std;
+
+#define TIME_UPDATEDATA 5  //hours
+
+vector<string> splitEx(const string& src, const string &separate_character);
+
+#endif //COMMON_H
